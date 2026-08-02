5539af699ecc1f2f771c09d0ce38977f2b3da6ee
juan mejia <juan.mejia@spidersoftware.co>
2026-08-02 18:13:04 -0500

    google

--- config/google.php
+++ config/google.php
@@ -3,8 +3,8 @@
 declare(strict_types=1);
 
 return [
-    'client_id' => '773851556264-u0631490co6cf9p9k9dfpbsojh7fr48q.apps.googleusercontent.com',
-    'client_secret' => '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',
+    'client_id' => 'id',
+    'client_secret' => 'id',
     'redirect_uri' => 'http://localhost/gob360/google/oauth-callback.php',
     'calendar_id' => 'primary',
     'timezone' => 'America/Bogota',
